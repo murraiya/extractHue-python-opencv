0885dfb3a86cb393d674d4eb84f91282e8be5a5d
murraiya <kimsuji0407@gmail.com>
2022-09-26 23:13:07 +0900

    edit_byungsin

--- skittles_hue copy.py
+++ skittles_hue copy.py
@@ -2,24 +2,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-skittles = cv2.imread('C:/Users/suji/Desktop/HOME/imageSignalProcessing/images/skittles.jpg')
+graph = cv2.imread('C:/Users/suji/Desktop/HOME/blue.jpg')
 # print(skittles.shape)
-cv2.imshow('bgr_image', skittles)
+cv2.imshow('bgr_image', graph)
 
-h,s,v=cv2.split(cv2.cvtColor(skittles, cv2.COLOR_BGR2HSV))
+h,s,v=cv2.split(cv2.cvtColor(graph, cv2.COLOR_BGR2HSV))
 # cv2.imshow('h', h)
-
 
 height, width = h.shape
 output=np.zeros((height, width), np.uint8)
 for i in range(0,height,1):
     for j in range(0,width,1):
-        if h[i,j]>=45 and h[i,j]<=75:
+        if h[i,j]>=110 and h[i,j]<=120:
             output[i,j]=255
         else:
             output[i,j]=0
 
-cv2.imshow('green_hue_over45_under75', output)
+cv2.imshow('getting_blue', output)
 
 # h_=(h>45 and h<75)*255
 # h_green=cv2.convertScaleAbs(h_) #int32->uint8
